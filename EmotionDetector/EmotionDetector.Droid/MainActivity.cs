ab900515d7a97d001644a30fb4cf0ce5a58e3bbb
Malte Götz <maltegoetz@outlook.com>
2015-11-15 02:58:39 +0100

    finished android

--- EmotionDetector/EmotionDetector.Droid/MainActivity.cs
+++ EmotionDetector/EmotionDetector.Droid/MainActivity.cs
@@ -15,10 +15,11 @@ using EmotionDetector.Droid.Common;
 using System.IO;
 using System.Threading.Tasks;
 using Xamarin.Media;
+using Android.Graphics.Drawables;
 
 namespace EmotionDetector.Droid
 {
-    [Activity(Label = "Emotion Detector", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Emotion Detector", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : Activity
     {
         ImageView _imgView;
@@ -47,10 +48,6 @@ namespace EmotionDetector.Droid
         }
         private void TakeAPicture(object sender, EventArgs eventArgs)
         {
-            //Intent intent = new Intent(MediaStore.ActionImageCapture);
-            //App._file = new Java.IO.File(App._dir, String.Format("myPhoto_{0}.jpg", Guid.NewGuid()));
-            //intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(App._file));
-
             var picker = new MediaPicker(this);
 
             var intent = picker.GetTakePhotoUI(new StoreCameraMediaOptions
@@ -73,11 +70,65 @@ namespace EmotionDetector.Droid
 
                 MediaFile file = await data.GetMediaFileExtraAsync(this);
 
-                _imgView.SetImageBitmap(BitmapFactory.DecodeFile(file.Path));
+                var rellayout = FindViewById<RelativeLayout>(Resource.Id.relativeLayout1);
+                for (int i = 2; i < rellayout.ChildCount; i++)
+                {
+                    rellayout.RemoveViewAt(i);
+                }
+                var progressBar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+                progressBar.Visibility = ViewStates.Visible;
+
+                var bmp = BitmapFactory.DecodeFile(file.Path);
+                _imgView.SetImageBitmap(bmp);
                 
                 await _vm.Load(file.GetStream());
+                
+                float scale;
+
+                if (_imgView.Width / bmp.Width >= _imgView.Height / bmp.Height)
+                {
+                    //Hochformat
+                    scale = (float)bmp.Height / _imgView.Height;
+                    //scale = imageFrame.Width / imageSize.Width;
+                }
+                else
+                {
+                    //Querformat
+                    scale = (float)bmp.Width / _imgView.Width;
+                    //scale = imageFrame.Height / imageSize.Height;
+                }
+
+                var relX = (_imgView.Width - bmp.Width / scale) / 2;
+                var relY = (_imgView.Height - bmp.Height / scale) / 2;
+                foreach(Emotion.Contract.Emotion emo in _vm.Emotions)
+                {
+                    var butt = new Button(this);
+                    GradientDrawable drawable = new GradientDrawable();
+                    drawable.SetShape(ShapeType.Rectangle);
+                    drawable.SetStroke(5, Color.Rgb(34,135,202));
+                    drawable.SetColor(Color.Transparent);
+                    butt.Background = drawable;
+                    var layoutparams = new RelativeLayout.LayoutParams((int)Math.Ceiling(emo.FaceRectangle.Width / scale), (int)Math.Ceiling(emo.FaceRectangle.Height / scale));
+                    layoutparams.SetMargins((int)Math.Ceiling(emo.FaceRectangle.Left / scale + relX), (int)Math.Ceiling(emo.FaceRectangle.Top / scale + relY), 0, 0);
+                    
+                    butt.LayoutParameters = layoutparams;
+                    butt.SetPadding(0, 0, 0, 0);
+                    butt.Click += (e, s) =>
+                    {
+                        (FindViewById<ProgressBar>(Resource.Id.angerProgressBar)).Progress = (int)Math.Ceiling(emo.Scores.Anger * 100);
+                        (FindViewById<ProgressBar>(Resource.Id.contemptProgressBar)).Progress = (int)Math.Ceiling(emo.Scores.Contempt * 100);
+                        (FindViewById<ProgressBar>(Resource.Id.disgustProgressBar)).Progress = (int)Math.Ceiling(emo.Scores.Disgust * 100);
+                        (FindViewById<ProgressBar>(Resource.Id.fearProgressBar)).Progress = (int)Math.Ceiling(emo.Scores.Fear * 100);
+                        (FindViewById<ProgressBar>(Resource.Id.happinessProgressBar)).Progress = (int)Math.Ceiling(emo.Scores.Happiness * 100);
+                        (FindViewById<ProgressBar>(Resource.Id.neutralProgressBar)).Progress = (int)Math.Ceiling(emo.Scores.Neutral * 100);
+                        (FindViewById<ProgressBar>(Resource.Id.sadnessProgressBar)).Progress = (int)Math.Ceiling(emo.Scores.Sadness * 100);
+                        (FindViewById<ProgressBar>(Resource.Id.surpriseProgressBar)).Progress = (int)Math.Ceiling(emo.Scores.Surprise * 100);
+                    };
+                    rellayout.AddView(butt);
+                    progressBar.Visibility = ViewStates.Invisible;
+                }
             }
-            catch (Exception)
+            catch (Exception e)
             {
                 
             }
--- EmotionDetector/EmotionDetector.Droid/Resources/Resource.designer.cs
+++ EmotionDetector/EmotionDetector.Droid/Resources/Resource.designer.cs
@@ -60,11 +60,68 @@ namespace EmotionDetector.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int imageView1 = 2131034112;
+			// aapt resource value: 0x7f050004
+			public const int GridLayout1 = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int angerProgressBar = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int contemptProgressBar = 2131034120;
+			
+			// aapt resource value: 0x7f05000a
+			public const int disgustProgressBar = 2131034122;
+			
+			// aapt resource value: 0x7f05000c
+			public const int fearProgressBar = 2131034124;
+			
+			// aapt resource value: 0x7f05000e
+			public const int happinessProgressBar = 2131034126;
 			
 			// aapt resource value: 0x7f050001
-			public const int myButton = 2131034113;
+			public const int imageView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int myButton = 2131034115;
+			
+			// aapt resource value: 0x7f050010
+			public const int neutralProgressBar = 2131034128;
+			
+			// aapt resource value: 0x7f050002
+			public const int progressBar1 = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int relativeLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050012
+			public const int sadnessProgressBar = 2131034130;
+			
+			// aapt resource value: 0x7f050014
+			public const int surpriseProgressBar = 2131034132;
+			
+			// aapt resource value: 0x7f050005
+			public const int textView1 = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int textView2 = 2131034119;
+			
+			// aapt resource value: 0x7f050009
+			public const int textView3 = 2131034121;
+			
+			// aapt resource value: 0x7f05000b
+			public const int textView4 = 2131034123;
+			
+			// aapt resource value: 0x7f05000d
+			public const int textView5 = 2131034125;
+			
+			// aapt resource value: 0x7f05000f
+			public const int textView6 = 2131034127;
+			
+			// aapt resource value: 0x7f050011
+			public const int textView7 = 2131034129;
+			
+			// aapt resource value: 0x7f050013
+			public const int textView8 = 2131034131;
 			
 			static Id()
 			{
